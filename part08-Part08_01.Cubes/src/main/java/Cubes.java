91a305a6cd3b61bfd5b35722c198092440e607de
cli16 <cli16@mail.wou.edu>
2020-03-30 23:00:01 -0700

    finished part8-1: Cube

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -1,10 +1,31 @@
 
 import java.util.Scanner;
 
+/*
+Write a program that reads strings from the user until the user inputs the string "end". 
+As long as the input is not "end" the program should handle the input as an integer 
+and print the cube of the integer (meaning number * number * number). 
+Below are some sample outputs
+
+ */
 public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            //read user inputs
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                //stop the program
+                break;
+            }
+
+            //convert the input String into a integer & return the result
+            int result = Integer.valueOf(input);
+            System.out.println((result * result * result));
+        }
 
     }
 }
