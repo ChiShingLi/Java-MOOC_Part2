54374ee185820b1a5e7bc79ed3ef7a7c523a4740
cli16 <cli16@mail.wou.edu>
2020-04-01 22:30:21 -0700

    finished 8-6 nickname hasmap

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -1,10 +1,23 @@
 
 import java.util.HashMap;
 
+/*
+matthew's nickname is matt
+michael's nickname is mix
+arthur's nickname is artie
+ */
 public class Nicknames {
 
     public static void main(String[] args) {
-        // Do the operations required here!
+        //create a name HashMap with the person's name and their nickname
+        HashMap<String, String> name = new HashMap<>();
+        name.put("matthew", "matt");
+        name.put("michael", "mix");
+        name.put("arthur", "artie");
+
+        //get Matthew's nickname from the HashMap and print it
+        System.out.println(name.get("matthew"));
+
     }
 
 }
