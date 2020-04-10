0f42642307b520237dc09034934bff92673e8fc1
cli16 <cli16@mail.wou.edu>
2020-04-09 23:37:53 -0700

    finished 9-9 returning Set object by passing in HashSet object

--- part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
+++ part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -1,13 +1,25 @@
 
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
         // You can test your method here
+        Set<String> set = new HashSet<>();
+        set.add("one");
+        set.add("one");
+        set.add("two");
 
+        for (String element : set) {
+            System.out.println(element);
+        }
     }
 
     // implement the method returnSize here, which returns
     // the number of elements in the set that it receives as a parameter.
+    public static int returnSize(Set setObj) {
+        return setObj.size();
+    }
 
 }
