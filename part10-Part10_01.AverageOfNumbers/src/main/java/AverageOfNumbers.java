625be42ddc9c6e0b319d9ec818bc6cf2c30a47de
cli16 <cli16@mail.wou.edu>
2020-04-12 15:40:01 -0700

    finished 10-1 calculate average number using Stream

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,4 +1,10 @@
-
+/*
+reads user input. If the user input is "end", 
+the program stops reading input.
+The rest of the input is numbers. 
+When the user input is "end", the program prints the average of all of the numbers.
+Implement calculating the average using a stream
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,5 +14,27 @@ public class AverageOfNumbers {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
 
+        ArrayList<String> wordList = new ArrayList<>();
+
+        while (true) {
+            //get user input as String
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            //else add the String in the ArrayLst
+            wordList.add(input);
+        }
+
+        // calculate the average using stream
+        double average = wordList.stream()
+                .mapToInt(s -> Integer.valueOf(s)) //convert string to int
+                .average() //calculate average
+                .getAsDouble(); //return result as double
+
+        System.out.println("average of the numbers: " + average);
     }
+
 }
