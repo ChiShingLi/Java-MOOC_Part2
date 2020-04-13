23405160da3783f4d5b85c0c508b49824617cd13
cli16 <cli16@mail.wou.edu>
2020-04-13 16:04:37 -0700

    finished 10-6 print certain range numbers using stream & lambda

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,4 +1,9 @@
-
+/*
+Write a program that reads user input. When the user gives a negative number as an input, 
+the input reading will be stopped. 
+After this,
+print all the numbers the user has given as input that are between 1 and 5.
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,6 +11,24 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numList = new ArrayList();
 
+        while (true) {
+            //convert String to integer
+            int input = Integer.valueOf(scanner.nextLine());
+
+            // stop if input is negative number
+            if (input < 0) {
+                break;
+            }
+
+            numList.add(input);
+        }
+        //print all the numbers that are between 1-5 using stream
+        numList.stream()
+                .filter(x -> (x <= 5)) //filter & get all the value between 1-5
+                .forEach(x -> { //print each values
+                    System.out.println(x);
+                });
     }
 }
