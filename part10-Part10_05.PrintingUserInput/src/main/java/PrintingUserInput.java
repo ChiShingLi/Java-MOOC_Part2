49e2fb0809fa843b284eaa86718626c764e7c127
cli16 <cli16@mail.wou.edu>
2020-04-13 15:50:55 -0700

    finished 10-5: printing use inputs with stream & lambda function

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,4 +1,9 @@
-
+/*
+Write a program that reads the user's input as strings.
+When the user inputs an empty string (only presses enter), 
+the input reading will be stopped and the program will print all the user inputs.
+using stream
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,6 +11,22 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputList = new ArrayList();
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            //add the user input to the list
+            inputList.add(input);
+        }
+
+        //for each input Strings in the stream print out each inputs
+        inputList.stream()
+                .forEach(x -> {
+                    System.out.println(x);
+                });
     }
 }
